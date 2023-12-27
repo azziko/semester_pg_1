db3ad1eb41592d550eada7816f65bdbe84d1ff6f
Aziz <sharipov.wdev@gmail.com>
2023-12-27 12:30:30 +0100

    route complete

--- handler/core.py
+++ handler/core.py
@@ -20,8 +20,14 @@ class Handler:
             self.client.send_options(chat_id)
 
         if text.startswith('/play_online'):
-            # do not create a player if already playing
-            player = game.Player(chat_id)
+            player = mm.get_player(chat_id)
+            if player == None:
+                player = game.Player(chat_id)
+                mm.players_db(player)
+
+            if player.opponent != None:
+                return self.client.send_message(chat_id, 'Already matched!')
+
             joined = mm.join_matchmaking(player)
 
             if joined != None:
@@ -33,8 +39,19 @@ class Handler:
                 self.client.send_options(joined.opponent.chat_id)
             else: 
                 self.client.send_message(chat_id, 'Waiting for the worthy opponent')
+                self.client.send_options(chat_id)
         
         if text.startswith('Close'):
-            self.client.delete_options(chat_id, 'Closed')
+            player = mm.get_player(chat_id)
+            if player != None:
+                mm.leave_matchmaking(player)
+                player_id, opponent_id = mm.disconnect_players(player)
+                if opponent_id != -1:
+                    self.client.delete_options(player_id, 'You were disconnected')
+                    self.client.delete_options(opponent_id, 'Your opponents left the match')
+                else: 
+                    self.client.delete_options(player_id, 'Closed')
+            else: 
+                self.client.delete_options(player_id, 'Closed')
 
         return '', 200
--- logic/core.py
+++ logic/core.py
@@ -9,26 +9,25 @@ win_combs = {
 class Player:
     def __init__(self, chat_id):
         self.id = chat_id
-        self.opponent = False
+        self.opponent = None
         self.choice = None
 
 class Matchmaker:
     def __init__(self):
-        self.players_pool = []
+        self.players_db = []
+        self.players_waiting = []
     
     def join_matchmaking(self, player):
-        # change it such that if player is not already playing, return none and not append
-        # And if already searching return none and not append
-        if player not in self.players_pool and player.opponent is None:
-            self.players_pool.append(player)
+        if player not in self.players_waiting and player.opponent is None:
+            self.players_waiting.append(player)
             return self.match_players()   
 
         return None 
 
     def match_players(self):
-        if len(self.players_pool) >= 2:
-            player1, player2 = self.players_pool[:2]
-            self.players_pool = self.players_pool[2:]
+        if len(self.players_waiting) >= 2:
+            player1, player2 = self.players_waiting[:2]
+            self.players_waiting = self.players_waiting[2:]
 
             player1.opponent = player2
             player2.opponent = player1
@@ -38,24 +37,36 @@ class Matchmaker:
         return None
 
     def leave_matchmaking(self, player):
-        if player in self.players_pool:
-            self.players_pool.remove(player)
+        if player in self.players_waiting:
+            self.players_waiting.remove(player)
 
     def disconnect_players(self, player):
-        if player.opponent:
-            self.notify_disconnect(player.opponent)
+        '''
+        Disconnects player and its opponent 
+        returning ids of both or -1, -1 otherwise.
+        '''
+
+        opponent_id = -1
+        player_id = -1
+        if player.opponent != None:
+            opponent_id = player.opponent.chat_id
+            player_id = player.chat_id
+
             player.opponent.opponent = None
             player.opponent = None
 
-    def notify_disconnect(self, player):
-        pass
+        return player_id, opponent_id
 
     def get_player(self, chat_id):
-        for player in self.players_waiting:
+        '''
+        Searches player by id in db and returns one if exists
+        None otherwise
+        '''
+
+        for player in self.players_db:
             if player.id == chat_id:
                 return player
         return None
-
 
 def determine_winner(player1, player2):
     if player1 == player2:
